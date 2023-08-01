900925c7a2a8596d89f9526750051b4aef0685ba
Kyle Khai Tran <kylekhai04@gmail.com>
2023-08-01 15:58:06 -0400

    Add support for other http request

--- restapp/src/main/java/com/kylekhai/restapp/Greeting_Controller.java
+++ restapp/src/main/java/com/kylekhai/restapp/Greeting_Controller.java
@@ -3,6 +3,8 @@ package com.kylekhai.restapp;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,8 @@ public class Greeting_Controller {
     private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
     
-    @GetMapping("/greeting")
+    //Support different http method types
+    @RequestMapping(method = RequestMethod.GET, path = "/greeting") 
     Greeting greeting(@RequestParam(defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
